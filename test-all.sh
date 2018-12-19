58ed674df0cbc627b128a73cfea028322a0c36bb
Marco Lovato <maglovato@gmail.com>
2018-12-19 10:33:09 -0200

    working version

--- test-all.sh
+++ test-all.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 set -e
-set -x
+# set -x
 echo
 echo Testing BOB
 echo ================================
 echo
 docker run --net=host lovato/bob /bin/bash -c "lsb_release -a && pip --version && echo -n 'npm ' && npm --version"
 echo
-echo Testing BOB-CPP
+echo Testing BOB-CPP additions to BOB
 echo ================================
 echo
 docker run --net=host lovato/bob-cpp /bin/bash -c "gcc --version && cmake --version"
 echo
-echo Testing BOB-PYTHON
+echo Testing BOB-PYTHON additions to BOB
 echo ================================
 echo
 docker run --net=host lovato/bob-python /bin/bash -c "python2 --version && python3 --version && pipenv --version"
-# echo
-# echo Testing BOB-NODEJS
-# echo ================================
-# echo
-# ./nodejs/run.sh "echo -n 'nodejs ' && node --version"
-# echo
-# echo Testing BOB-JAVA
-# echo ================================
-# echo
-# ./java/run.sh "java -version && mvn -v && ant -version"
-# echo
-# echo Testing BOB-ANDROID
-# echo ================================
-# echo
-# ./android/run.sh "gradle -v && sdkmanager --list"
-# echo
-# echo Testing BOB-ANDROID-NDK
-# echo ================================
-# echo
-# ./android/ndk/run.sh "sdkmanager --list"
-# echo
+echo
+echo Testing BOB-NODEJS additions to BOB
+echo ================================
+echo
+./nodejs/run.sh "echo -n 'nodejs ' && node --version"
+echo
+echo Testing BOB-JAVA additions to BOB
+echo ================================
+echo
+./java/run.sh "java -version && mvn -v && ant -version"
+echo
+echo Testing BOB-ANDROID additions to BOB-JAVA
+echo ================================
+echo
+./android/run.sh "gradle -v && sdkmanager --list"
+echo
+echo Testing BOB-ANDROID-NDK additions to BOB-ANDROID
+echo ================================
+echo
+./android/ndk/run.sh "sdkmanager --list"
+echo
