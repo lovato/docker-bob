19d5b7e6104a917f35135421815c9a6480a09368
Marco Lovato <maglovato@gmail.com>
2019-02-14 09:06:14 -0200

    -e AWS_REGION to all run.sh scripts

--- java/run.sh
+++ java/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 if [[ $# -eq 0 ]] ; then
-  docker run -it --net=host -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -v ~/:/userhost:ro lovato/bob-java
+  docker run -it --net=host -e AWS_REGION -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -v ~/:/userhost:ro lovato/bob-java
 else
-  docker run --net=host -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY lovato/bob-java /bin/bash -c "$1"
+  docker run --net=host -e AWS_REGION -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY lovato/bob-java /bin/bash -c "$1"
 fi
